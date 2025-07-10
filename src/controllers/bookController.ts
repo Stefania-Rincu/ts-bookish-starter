313cf6cd46a72fd12b5e16252402731cedf427ad
Stefania Rincu <Stefania.Rincu@softwire.com>
2025-07-10 10:42:04 +0300

    Create endpoint to get all books

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -24,18 +24,36 @@ class BookController {
 
     getAllBooks(req: Request, res: Response) {
         const books: Book[] = [];
-        const selectQuery: string = 'SELECT * FROM BOOKS';
+        const selectQuery: string = 'SELECT * FROM dbo.BOOK';
 
-        const selectRequest = new TediousRequest(
-            selectQuery,
-            (error, rowCount) => {
-                try {
-                    console.log(rowCount);
-                } catch (error) {
-                    console.log(error);
-                }
-            },
-        );
+        const selectRequest = new TediousRequest(selectQuery, (error) => {
+            if (error) {
+                console.log(`Error getting all books. Error message ${error}`);
+                return res
+                    .status(500)
+                    .json({ error: 'Database SELECT * FROM BOOKS failed!' });
+            }
+        });
+
+        selectRequest.on('row', (columns) => {
+            const bookData = {};
+            columns.forEach((column) => {
+                bookData[column.metadata.colName] = column.value;
+            });
+
+            const book: Book = new Book(
+                bookData['id'],
+                bookData['title'],
+                bookData['isbn'],
+                bookData['num_copies'],
+            );
+
+            books.push(book);
+        });
+
+        selectRequest.on('requestCompleted', () => {
+            return res.json(books);
+        });
 
         connection.execSql(selectRequest);
     }
