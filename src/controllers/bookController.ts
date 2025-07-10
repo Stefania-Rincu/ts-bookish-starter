b8061a10bb72bd142f7bb7388c8500fec4fe2d20
Stefania Rincu <Stefania.Rincu@softwire.com>
2025-07-10 11:04:26 +0300

    Create endpoint to get book by id

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { Request as TediousRequest } from 'tedious';
+import { Request as TediousRequest, TYPES } from 'tedious';
 import { Book } from '../models/book';
 import { connection } from '../databaseConnection';
 
@@ -15,11 +15,48 @@ class BookController {
     }
 
     getBook(req: Request, res: Response) {
-        // TODO: implement functionality
-        return res.status(500).json({
-            error: 'server_error',
-            error_description: 'Endpoint not implemented yet.',
+        const bookId: number = parseInt(req.params.id);
+        const selectQuery: string = 'SELECT * FROM dbo.BOOK WHERE id = @id';
+        let book: Book = null;
+
+        const selectRequest = new TediousRequest(selectQuery, (error) => {
+            if (error) {
+                console.log(
+                    `Error getting book with id = ${bookId}. Error message ${error}`,
+                );
+                return res.status(500).json({
+                    error: `Database SELECT * FROM BOOK WHERE id = ${bookId} failed!`,
+                });
+            }
         });
+
+        selectRequest.addParameter('id', TYPES.Int, bookId);
+
+        selectRequest.on('row', (columns) => {
+            const bookData = {};
+            columns.forEach((column) => {
+                bookData[column.metadata.colName] = column.value;
+            });
+
+            book = new Book(
+                bookData['id'],
+                bookData['title'],
+                bookData['isbn'],
+                bookData['num_copies'],
+            );
+
+            res.json(book);
+        });
+
+        selectRequest.on('requestCompleted', () => {
+            if (book === null)
+                res.status(404).json({
+                    error: 'not_found',
+                    error_description: `Book with id = ${bookId} does not exist`,
+                });
+        });
+
+        connection.execSql(selectRequest);
     }
 
     getAllBooks(req: Request, res: Response) {
@@ -31,7 +68,7 @@ class BookController {
                 console.log(`Error getting all books. Error message ${error}`);
                 return res
                     .status(500)
-                    .json({ error: 'Database SELECT * FROM BOOKS failed!' });
+                    .json({ error: 'Database SELECT * FROM BOOK failed!' });
             }
         });
 
